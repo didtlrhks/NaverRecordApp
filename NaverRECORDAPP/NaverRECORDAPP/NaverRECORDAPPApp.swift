df8f9153f11c5ab5b9ee4c5ddfa7973145722970
sikwan <yysskk99999@naver.com>
2024-01-05 19:31:25 +0900

    first commit

---
+++ NaverRECORDAPP/NaverRECORDAPP/NaverRECORDAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaverRECORDAPPApp.swift
+//  NaverRECORDAPP
+//
+//  Created by 양시관 on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct NaverRECORDAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
