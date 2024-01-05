aef72064a040b855bfe6a357e27088a20cda7bcd
sikwan <yysskk99999@naver.com>
2024-01-05 19:46:47 +0900

    ColorExtenstions

---
+++ NaverRECORDAPP/NaverRECORDAPP/Color+Extensions.swift
@@ -0,0 +1,26 @@
+//
+//  Color+Extensions.swift
+//  NaverRECORDAPP
+//
+//  Created by 양시관 on 1/5/24.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension Color {
+    
+    static let customBackgroundGreen = Color("Green")
+    static let customBlack = Color("Black")
+    static let customCoolGray = Color("YellowBlue")
+    static let customGray0 = Color("Gray0")
+    static let customGray1 = Color("Gray1")
+    static let customGray2 = Color("Gray2")
+    static let customGreen = Color("customGreen")
+    static let customIconGray = Color("Gray3")
+    static let customOrange = Color("Orange")
+    static let customSky = Color("SkyBlue")
+    static let customWhite = Color("White")
+    
+}
