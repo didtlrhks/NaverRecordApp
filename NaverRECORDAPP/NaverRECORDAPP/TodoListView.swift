429c886f9325cc84fe6656d6144245ae1df25456
sikwan <yysskk99999@naver.com>
2024-01-07 23:16:33 +0900

    TodoListView

---
+++ NaverRECORDAPP/NaverRECORDAPP/TodoListView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoListView.swift
+//  NaverRECORDAPP
+//
+//  Created by 양시관 on 1/7/24.
+//
+
+import SwiftUI
+
+struct TodoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoListView()
+}
