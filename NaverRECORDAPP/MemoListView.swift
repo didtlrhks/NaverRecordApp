07bb080c932f877c7b26fae66ab1cbe12df11006
sikwan <yysskk99999@naver.com>
2024-01-08 18:46:17 +0900

    Announcement View

--- NaverRECORDAPP/MemoListView.swift
+++ NaverRECORDAPP/MemoListView.swift
@@ -29,7 +29,7 @@ struct MemoListView: View {
                         .frame(height: 30)
                     
                 }
-                //title view
+                TitleView()
                 
                     //안내뷰 혹은 메모리스트 컨텐츠 뷰
                 
@@ -40,6 +40,47 @@ struct MemoListView: View {
         }
 }
 
+private struct TitleView : View {
+    @EnvironmentObject private var memoListViewModel : MemoListViewModel
+    
+    fileprivate var body: some View {
+        HStack{
+            if memoListViewModel.memos.isEmpty {
+                Text("메모를\n추가해 보세요.")
+            }else {
+                Text("메모 \(memoListViewModel.memos.count)개가 \n있습니다.")
+            }
+            Spacer()
+        }
+        .font(.system(size:30,weight: .bold))
+        .padding(.leading, 20)
+    }
+}
+
+private struct AnnouncementView:View {
+    fileprivate var body: some View {
+        VStack(spacing : 15) {
+            Spacer()
+            
+            Image("pencil")
+                .renderingMode(.template)
+            Text("\"퇴근 9시간전 메모\"")
+            Text("\"개발 끝낸 후 퇴근하기!\"")
+            Text("\"밀린 알고리즘 공부하기!\"")
+            
+            Spacer()
+            
+            
+        }
+        .font(.system(size:16))
+        .foregroundColor(.customGray2)
+    }
+}
+
+
+
 #Preview {
     MemoListView()
+        .environmentObject(PathModel())
+        .environmentObject(MemoListViewModel())
 }
