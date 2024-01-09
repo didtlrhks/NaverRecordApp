e8ab87c660612876132b3c9e95eb7f6450db221f
sikwan <yysskk99999@naver.com>
2024-01-09 19:17:16 +0900

    voice recorder View Model

--- NaverRECORDAPP/NaverRECORDAPP/VoiceRecorderViewModel.swift
+++ NaverRECORDAPP/NaverRECORDAPP/VoiceRecorderViewModel.swift
@@ -8,96 +8,212 @@
 import Foundation
 import AVFoundation
 
-class VoiceRecorderViewModel : NSObject, ObservableObject, AVAudioPlayerDelegate {
-    @Published var isDisPlayRemoveVoiceRecorderAlert : Bool
-    @Published var isDisPlayErrorAlert : Bool
-    @Published var errorAlertMessage : String
-    
-    
-    
-    var audioRecorder : AVAudioRecorder?
-    @Published var isRecording : Bool
-    
-    var audioPlayer : AVAudioPlayer?
-    @Published var isPlaying : Bool
-    @Published var isPaused : Bool
-    @Published var playedTime : TimeInterval
-    private var progressTimer : Timer?
-    
-    var recordedFiles : [URL]
-    
-    @Published var selectedRecoredFile: URL?
-    
-    init(isDisPlayRemoveVoiceRecorderAlert: Bool = false,
-         isDisPlayErrorAlert: Bool  = false,
-         errorAlertMessage: String = "",
-         isRecording: Bool = false,
-         audioPlayer: AVAudioPlayer? = nil,
-         isPlaying: Bool = false,
-         isPaused: Bool = false,
-         playedTime: TimeInterval,
-         recordedFiles: [URL],
-         selectedRecoredFile: URL? = nil
-    ) {
-        self.isDisPlayRemoveVoiceRecorderAlert = isDisPlayRemoveVoiceRecorderAlert
-        self.isDisPlayErrorAlert = isDisPlayErrorAlert
-        self.errorAlertMessage = errorAlertMessage
-        self.isRecording = isRecording
-        self.audioPlayer = audioPlayer
-        self.isPlaying = isPlaying
-        self.isPaused = isPaused
-        self.playedTime = playedTime
-        self.recordedFiles = recordedFiles
-        self.selectedRecoredFile = selectedRecoredFile
-    }
+
+
+class VoiceRecorderViewModel: NSObject, ObservableObject, AVAudioPlayerDelegate {
+  @Published var isDisplayRemoveVoiceRecorderAlert: Bool
+  @Published var isDisplayAlert: Bool
+  @Published var alertMessage: String
+  
+  /// 음성메모 녹음 관련 프로퍼티
+  var audioRecorder: AVAudioRecorder?
+  @Published var isRecording: Bool
+  
+  /// 음성메모 재생 관련 프로퍼티
+  var audioPlayer: AVAudioPlayer?
+  @Published var isPlaying: Bool
+  @Published var isPaused: Bool
+  @Published var playedTime: TimeInterval
+  private var progressTimer: Timer?
+  
+  /// 음성메모된 파일
+  var recordedFiles: [URL]
+  
+  /// 현재 선택된 음성메모 파일
+  @Published var selectedRecoredFile: URL?
+  
+  init(
+    isDisplayRemoveVoiceRecorderAlert: Bool = false,
+    isDisplayErrorAlert: Bool = false,
+    errorAlertMessage: String = "",
+    isRecording: Bool = false,
+    isPlaying: Bool = false,
+    isPaused: Bool = false,
+    playedTime: TimeInterval = 0,
+    recordedFiles: [URL] = [],
+    selectedRecoredFile: URL? = nil
+  ) {
+    self.isDisplayRemoveVoiceRecorderAlert = isDisplayRemoveVoiceRecorderAlert
+    self.isDisplayAlert = isDisplayErrorAlert
+    self.alertMessage = errorAlertMessage
+    self.isRecording = isRecording
+    self.isPlaying = isPlaying
+    self.isPaused = isPaused
+    self.playedTime = playedTime
+    self.recordedFiles = recordedFiles
+    self.selectedRecoredFile = selectedRecoredFile
+  }
 }
 
 extension VoiceRecorderViewModel {
-    func voiceRecordCellTapped(_ recordedFile: URL) {
-        if selectedRecoredFile != recordedFile {
-            // 재생정지 메서드 호출
-            selectedRecoredFile = recordedFile
-        }
+  func voiceRecordCellTapped(_ recordedFile: URL) {
+    if selectedRecoredFile != recordedFile {
+      stopPlaying()
+      selectedRecoredFile = recordedFile
+    }
+  }
+  
+  func removeBtnTapped() {
+    setIsDisplayRemoveVoiceRecorderAlert(true)
+  }
+  
+  func removeSelectedVoiceRecord() {
+    guard let fileToRemove = selectedRecoredFile,
+          let indexToRemove = recordedFiles.firstIndex(of: fileToRemove) else {
+      displayAlert(message: "선택된 음성메모 파일을 찾을 수 없습니다.")
+      return
     }
     
-    func removeBtnTapped() {
-        //삭제 얼럿 노출 메서드
-        setIsDisplayRemoveVoiceRecorderAlert(true)
+    do {
+      try FileManager.default.removeItem(at: fileToRemove)
+      recordedFiles.remove(at: indexToRemove)
+      selectedRecoredFile = nil
+      stopPlaying()
+      displayAlert(message: "선택된 음성메모 파일을 성공적으로 삭제했습니다.")
+    } catch {
+      displayAlert(message: "선택된 음성메모 파일 삭제 중 오류가 발생했습니다.")
+    }
+  }
+  
+  private func setIsDisplayRemoveVoiceRecorderAlert(_ isDisplay: Bool) {
+    isDisplayRemoveVoiceRecorderAlert = isDisplay
+  }
+  
+  private func setErrorAlertMessage(_ message: String) {
+    alertMessage = message
+  }
+  
+  private func setIsDisplayErrorAlert(_ isDisplay: Bool) {
+    isDisplayAlert = isDisplay
+  }
+  
+  private func displayAlert(message: String) {
+    setErrorAlertMessage(message)
+    setIsDisplayErrorAlert(true)
+  }
+}
+
+// MARK: - 음성메모 녹음 관련
+extension VoiceRecorderViewModel {
+  func recordBtnTapped() {
+    selectedRecoredFile = nil
+    
+    if isPlaying {
+      stopPlaying()
+      startRecording()
+    } else if isRecording {
+      stopRecording()
+    } else {
+      startRecording()
+    }
+  }
+  
+  private func startRecording() {
+    let fileURL = getDocumentsDirectory().appendingPathComponent("새로운 녹음 \(recordedFiles.count + 1)")
+    let settings = [
+      AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+      AVSampleRateKey: 12000,
+      AVNumberOfChannelsKey: 1,
+      AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+    ]
+    
+    do {
+      audioRecorder = try AVAudioRecorder(url: fileURL, settings: settings)
+      audioRecorder?.record()
+      self.isRecording = true
+    } catch {
+      displayAlert(message: "음성메모 녹음 중 오류가 발생했습니다.")
+    }
+  }
+  
+  private func stopRecording() {
+    audioRecorder?.stop()
+    self.recordedFiles.append(self.audioRecorder!.url)
+    self.isRecording = false
+  }
+  
+  private func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+  }
+}
+
+// MARK: - 음성메모 재생 관련
+extension VoiceRecorderViewModel {
+  func startPlaying(recordingURL: URL) {
+    do {
+      audioPlayer = try AVAudioPlayer(contentsOf: recordingURL)
+      audioPlayer?.delegate = self
+      audioPlayer?.play()
+      self.isPlaying = true
+      self.isPaused = false
+      self.progressTimer = Timer.scheduledTimer(
+        withTimeInterval: 0.1,
+        repeats: true
+      ) { _ in
+        self.updateCurrentTime()
+      }
+    } catch {
+      displayAlert(message: "음성메모 재생 중 오류가 발생했습니다.")
+    }
+  }
+  
+  private func updateCurrentTime() {
+    self.playedTime = audioPlayer?.currentTime ?? 0
+  }
+  
+  private func stopPlaying() {
+    audioPlayer?.stop()
+    playedTime = 0
+    self.progressTimer?.invalidate()
+    self.isPlaying = false
+    self.isPaused = false
+  }
+  
+  func pausePlaying() {
+    audioPlayer?.pause()
+    self.isPaused = true
+  }
+  
+  func resumePlaying() {
+    audioPlayer?.play()
+    self.isPaused = false
+  }
+  
+  func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+    self.isPlaying = false
+    self.isPaused = false
+  }
+  
+  func getFileInfo(for url: URL) -> (Date?, TimeInterval?) {
+    let fileManager = FileManager.default
+    var creationDate: Date?
+    var duration: TimeInterval?
+    
+    do {
+      let fileAttributes = try fileManager.attributesOfItem(atPath: url.path)
+      creationDate = fileAttributes[.creationDate] as? Date
+    } catch {
+      displayAlert(message: "선택된 음성메모 파일 정보를 불러올 수 없습니다.")
     }
     
-    func removeSelectedVoiceRecord() {
-        
-        guard let fileToRemove = selectedRecoredFile,
-              let indexToRemove = recordedFiles.firstIndex(of: fileToRemove) else {
-            displayAlert(message: "선택된 음성메모 파일을 찾을수 없습니다.")
-            return
-        }
-        
-        do {
-            try FileManager.default.removeItem(at: fileToRemove)
-            recordedFiles.remove(at: indexToRemove)
-            selectedRecoredFile = nil
-            displayAlert(message: "선택된 음성메모 파일을 성공적으로 삭제했습니다.")
-            //재생정지 매서드
-            //삭제성공 알럿
-            
-        }
-        catch {
-            //삭제 실패오류 얼럿
-            displayAlert(message: "선택된 음성메모 파일 삭제 중 오류가 발생했습니다.ㄴ")
-        }
+    do {
+      let audioPlayer = try AVAudioPlayer(contentsOf: url)
+      duration = audioPlayer.duration
+    } catch {
+      displayAlert(message: "선택된 음성메모 파일의 재생 시간을 불러올 수 없습니다.")
     }
-    private func setIsDisplayRemoveVoiceRecorderAlert(_ isDisplay : Bool) {
-        isDisPlayRemoveVoiceRecorderAlert = isDisplay
-    }
-    private func setErrorAlertMessage(_ message: String) {
-        errorAlertMessage = message
-    }
-    private func setIsDisplayErrorAlert(_ isDisplay : Bool) {
-        isDisPlayErrorAlert = isDisplay
-    }
-    private func displayAlert(message : String ) {
-        setErrorAlertMessage(message)
-        setIsDisplayErrorAlert(true)
-    }
+    
+    return (creationDate, duration)
+  }
 }
