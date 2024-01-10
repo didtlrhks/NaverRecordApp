acdcfcf00cdfd7cfb56d47000e1d2f500ec36447
sikwan <yysskk99999@naver.com>
2024-01-10 12:21:58 +0900

    timerViewModel

--- NaverRECORDAPP/NaverRECORDAPP/TimerViewModel.swift
+++ NaverRECORDAPP/NaverRECORDAPP/TimerViewModel.swift
@@ -14,16 +14,69 @@ class TimerViewModel : ObservableObject {
     @Published var timeRemaining : Int
     @Published var isPaused : Bool
     
-     init(isDisplaySetTimeView: Bool,
-          time: Time,
+     init(isDisplaySetTimeView: Bool = true,
+          time: Time = .init(hours: 0, minutes: 0, seconds: 0),
           timer: Timer? = nil,
-          timeRemaining: Int,
-          isPaused: Bool)
+          timeRemaining: Int = 0,
+          isPaused: Bool = false
+     )
     {
         self.isDisplaySetTimeView = isDisplaySetTimeView
         self.time = time
         self.timer = timer
         self.timeRemaining = timeRemaining
         self.isPaused = isPaused
+    }
+    
+    
+}
+
+extension TimerViewModel {
+    func settingBtnTapped() {
+        isDisplaySetTimeView = false
+        timeRemaining = time.convertedSeconds
+        // TODO: - 타이머 시작 메서드 호출
+        startTimer()
+    }
+    
+    func cacelBtnTapped() {
+        //TODO: - 타이머 종료 메서드 호출
+        stopTimer()
+        isDisplaySetTimeView = true
+    }
+    
+    func pauseOrRestartBtnTapped() {
+        if isPaused {
+            //TODO: - 타이머 시작 메서드 호출
+            startTimer()
+        }
+        else {
+            timer?.invalidate()
+            timer = nil
+        }
+        isPaused.toggle()
+    }
+}
+
+private extension TimerViewModel {
+    func startTimer() {
+        guard timer == nil else{ return }
+        
+        timer = Timer.scheduledTimer(
+            withTimeInterval : 1,
+            repeats: true
+        ) {
+            _ in
+            if self.timeRemaining > 0{
+                self.timeRemaining -= 1
+            }else {
+                self.stopTimer()
+            }
+        }
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
     }
 }
