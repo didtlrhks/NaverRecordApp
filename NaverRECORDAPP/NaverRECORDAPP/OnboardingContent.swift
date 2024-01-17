2b4e378d938e659c0f24dbcb9aa71d70a550f449
sikwan <yysskk99999@naver.com>
2024-01-18 00:15:53 +0900

    OnboardingViewModel 주석 달기

--- NaverRECORDAPP/NaverRECORDAPP/OnboardingContent.swift
+++ NaverRECORDAPP/NaverRECORDAPP/OnboardingContent.swift
@@ -9,19 +9,21 @@ import Foundation
 import SwiftUI
 
 
-struct OnboardingContent : Hashable{
+struct OnboardingContent : Hashable {  // Hashable 는 뭘까 이거를 왜씀? 결국 이녀석을 쓰는 이유자체는 객체를 해시 테이블 기반의 데이터 구조에서 효율적으로 사용할 수 있게 하기 위함이지.
+    //해시어블은 있잖아 일단 데이터구조를 디게 간단하게 만들어주는데 이게 key value 로 바뀌어가는느낌이거덩 해시함수를 거쳐서 배열에다가 인덱스를 맞춰서 값들을 넣어주는 역할을 함으로써 데이터의 구조화등에 대해서 이득이 된다. 해시테이블기반 구조 중요함
     
-    var imageFileName: String
+    
+    var imageFileName: String // 해당 구조쳉에서 쓰려고하는 녀석들을 선언해준거다
     var title : String
     var subTitle: String
     
     init(
-        imageFileName : String,
+        imageFileName : String, //초기화 잘 받아준거임 init 으로 잘해주고있고 이걸 근데 꼭 해줘야하나?
         title : String,
         subTitle: String
     )
     {
-        self.imageFileName = imageFileName
+        self.imageFileName = imageFileName //self 는 같은 클래스나 구조체안에서 변수들에 접근하기 위해서 사용하네 그래서 자기자신이라는 의믜에 self 를 써서 접근하는거구나
         self.title = title
         self.subTitle = subTitle
     }
--- NaverRECORDAPP/NaverRECORDAPP/OnboardingViewModel.swift
+++ NaverRECORDAPP/NaverRECORDAPP/OnboardingViewModel.swift
@@ -10,20 +10,23 @@ import Foundation
 
 
 
-class OnboardingViewModel : ObservableObject{
-    @Published var onboardingContents : [OnboardingContent]
+class OnboardingViewModel : ObservableObject{ // 이거는 뭐냐면 일단 클래스 선언한거고 ObservableObject 프로토콜을 왜 사용하는거냐면 온보딩 뷰모델을 구독하는 녀석들에 대한 데이터의 변화를 동적으로 관리해주는 녀석이야 이유는 일단 스위프트에 강점 인것도있지 왜냐면 선언형을 따르면서 MVVM 패턴을 유지해줄수있는 큰이유중에 하나거덩 이녀석이 데이터를 구독하고 있다가 여기에있는 데이터들이 다른곳에서 쓰이면 그걸 구독하고 업데이트하는역할을 하는거지  한마디로 이걸 사용하는 데이터들은 다른곳에서 데이터가 변하면 그걸 구독하고 있다가 데이터를 알려주는거지 MVVM 패턴에서 왜 유리하냐면 이런게있어야. 아니다 일단 우선적으로 MVVM 은 사용자의 뷰와 인터렉션과 데이터들을 구분해놓은 디자인 패턴인데 이런게 있기때문에 데이터는 바인딩해서 사용할 수 있는거고 뷰단에서는 그것만 사용할 수 있게되는거지
+    
+    @Published var onboardingContents : [OnboardingContent] // 퍼블리쉬드 가 뭐냐면 온보딩 컨텐트에 있는 데이터들이 있잖아 그럼 걔네들의 데이터를 사용하고 있다고 선언해놓는거야 그럼 여기서 보고있다가 위에 옵져버블 옵젝트에서 데이터의 변화를 느끼고 뷰에다가 데이터를 새로고침하는거지!
+    //그리고 여기에서 배열로 타입을 넣어줬기 떄문에 밑에 init 부분인.초기화 부분에서 그 행동을 해주는거야
+    
     
     init(
         onboardingContents: [OnboardingContent] = [
             .init(imageFileName: "onboarding_1", title: "오늘의 할일", subTitle: "To do list 로 언제 어디서든 한눈에"),
-            .init(imageFileName: "onboarding_2", title: "똑똑한 나만의 기록장", subTitle: "메모장으로 생각나는 기록은 언제든지"),
+            .init(imageFileName: "onboarding_2", title: "똑똑한 나만의 기록장", subTitle: "메모장으로 생각나는 기록은 언제든지"), //배열로 타입을 넣어줬기 떄문에 밑에 init 부분인.초기화 부분에서 그 행동을 해주는거야
             .init(imageFileName: "onboarding_3", title: "하나라도 놓치지 않도록", subTitle: "음성메모 기능으로 놓치고 싶지않은 기록까지"),
             .init(imageFileName: "onboarding_4", title: "정확한 시간의 경과", subTitle: "타이머 기능으로 원하는 시간을 확인")
         ]
     
     ) {
         
-        self.onboardingContents = onboardingContents
+        self.onboardingContents = onboardingContents //이거는 전에 나왔던거처럼 그냥 똑같이 같은 클래스나 구조체안에다가 자기자신의 값에 접근하고 싶을때 사용하는것이지 
         
     }
 }
