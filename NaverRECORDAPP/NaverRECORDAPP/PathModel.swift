b98e6ce5937f747d8938bb99ef6a60512307e1ad
sikwan <yysskk99999@naver.com>
2024-01-18 00:53:01 +0900

    Pathtype,PathModel 주석 달기

--- NaverRECORDAPP/NaverRECORDAPP/PathModel.swift
+++ NaverRECORDAPP/NaverRECORDAPP/PathModel.swift
@@ -8,10 +8,12 @@
 import Foundation
 
 
-class PathModel : ObservableObject {
-    @Published var paths : [PathType]
+class PathModel : ObservableObject { // 패쓰모델을 지정한것도 지정한것데 여기서 ObservavleObject를 사용하는 이유는 다른 모델에서 다른뷰에 데이터들을 확인하려고 하는거지
+    @Published var paths : [PathType] // 즉 여기서 PathType 을 데이터가변하는걸 받아와서 위에 옵저버블옵젝트에서 구독하고있는 녀석들의 데이터가 변하면 그값들을 뷰단에서 업데이트해주는 역할을 한다.
     
-    init(paths : [PathType] = []){
-        self.paths = paths
+    init(paths : [PathType] = []) // 초기화를 안해도 되는경우 생략이 가능한것을 얘기해, 그래서 그냥 초기값을 안주고 이렇게 해도 배열의 초기값 자체를 없다고 생각해도 되는듯
+    
+    {
+        self.paths = paths // 클래스 안에있는 자기자신에 변수에 접근할 수 있도록 구현한거임
     }
 }
--- NaverRECORDAPP/NaverRECORDAPP/PathType.swift
+++ NaverRECORDAPP/NaverRECORDAPP/PathType.swift
@@ -8,10 +8,11 @@
 import Foundation
 
 
-enum PathType: Hashable {
-    case homeView
-    case todoView
-    case memoView(isCreateMode : Bool , memo : Memo?)
+enum PathType: Hashable { // 이넘은 열거형으로 써본결과 가장 제한이없는 녀석인듯 음 그러니까 좀 편하게 같은 유형의 친구들을 모아놓는걸 의미한다잉
+    case homeView//이거는 지금이뷰만 봐서는 정확한 의미를 쓰긴힘들지만 다하고 와서 한번 더 볼듯 근데 강의를 보며 느낀거는 홈뷰로 가는 길을 지정해주려고 만들어준거임
+    case todoView//위와 같지만 이번엔 todoView 로 이동한다는 개념으로 구현해놈
+    case memoView(isCreateMode : Bool , memo : Memo?) // 이것도 다른애들과같기는한데 메모뷰에서 참조해야하는값들이 있나보네 그래서 이렇게 해놓은거같에 의미로 따지자면 메모를 만드는 뷰로 들어갈건지를 Bool 타입으로 지정해놓은거고 memo 는 메모가 있는지없는지도 판별해야되니까 ? 인 옵셔널로 표현해놓은거지
+    
     
     
 }
