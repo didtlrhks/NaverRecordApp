a0b3c8750b5ad109c18af42566b0aa22ef34af8f
sikwan <yysskk99999@naver.com>
2024-01-17 20:03:56 +0900

    주석 달기 메인 시작부

--- NaverRECORDAPP/NaverRECORDAPP/Color+Extensions.swift
+++ NaverRECORDAPP/NaverRECORDAPP/Color+Extensions.swift
@@ -9,9 +9,12 @@ import Foundation
 import SwiftUI
 
 
-extension Color {
+extension Color { // 왜 extension 을 썼지? : extension 은 원래있던 클래스나 이넘등의 경우에서 추가로 함수나 메서드등을 추가해줄수있는 녀석이네, 그래서 나는 색깔의 이름들을 새로 초기화해서 내가 쓰기편하게 하려고 이런거를 사용하는구나,
     
-    static let customBackgroundGreen = Color("Green")
+    
+    static let customBackgroundGreen = Color("Green") // 색ㄱ깔을 정의하는데 왜 static let 을 썻을까? : 일단 인스턴스를 만들필요가 없음, namespace는 또 제공을 함 그러니까 이런 색상등처럼 프로젝트 전역에 쓰여야할때 object를 다넘겨주지않아도 되니까 이렇게 진행하나봄
+    
+     
     static let customBlack = Color("Black")
     static let customCoolGray = Color("YellowBlue")
     static let customGray0 = Color("Gray0")
@@ -22,5 +25,7 @@ extension Color {
     static let customOrange = Color("Orange")
     static let customSky = Color("SkyBlue")
     static let customWhite = Color("White")
+    
+    
     
 }
--- NaverRECORDAPP/NaverRECORDAPP/NaverRECORDAPPApp.swift
+++ NaverRECORDAPP/NaverRECORDAPP/NaverRECORDAPPApp.swift
@@ -7,14 +7,17 @@
 
 import SwiftUI
 
-@main
+@main // 앱이 시작한다는 시작점을 알려주는
 struct NaverRECORDAPPApp: App {
     
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
+    //@ 부터 Adaptor 까지 기존 uikit 에 있던 기능들을 swiftui 에서 사용할 수 있도록 해놓은것이구만
+   // AppDelegate 클래스의 인스턴스를 appDelegate 라는 이름으로 만들어 SwiftUI 생명주기와 함께 동작하도록 하는거구만여
     var body: some Scene {
-        WindowGroup {
-            OnboardingView()
+        WindowGroup {// 여기서 앱메인 뷰를 그리고
+            OnboardingView() // 이게 어떤 뷰부터 시작할지를 정해주는거구만 ..
         }
+        
     }
 }
