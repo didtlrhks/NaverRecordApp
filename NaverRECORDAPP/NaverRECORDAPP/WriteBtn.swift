d97e78cff47e72019887ea9449a070ec5d95b0b5
sikwan <yysskk99999@naver.com>
2024-01-20 19:49:24 +0900

    WriteBtn closure 주석 처리

--- NaverRECORDAPP/NaverRECORDAPP/WriteBtn.swift
+++ NaverRECORDAPP/NaverRECORDAPP/WriteBtn.swift
@@ -9,7 +9,8 @@ import Foundation
 import SwiftUI
 
 extension View {
-    public func writeBtn(perform action: @escaping () -> Void) -> some View {
+    public func writeBtn(perform action: @escaping () -> Void) -> some View { // 그냥 사실 말이 클로저지 함수한테 이름이 있냐 없냐 이정도의 차이를 가지고 있는거고 즉 코드 블럭이라고 생각하면됨 필요한경우에는 주변에서 캡처된 변수를 사용을 할수 있다가 클로저의 한줄요약같은느낌인데
+        //여기서는 action 에 @escaping 가 있는데 이게뭐냐면 이 클로저는 함수의 실행범위를 벗어난 후에도 호출될 수가 있다는걸 의미하는거여 예를들어 비동기 작업이나 이벤트나 사용자 인터렉션이있을때 실행될수있는거지 이 버튼도 사실이고 쓰기버튼이니까
         ZStack{
             self
             
@@ -28,3 +29,4 @@ extension View {
             .padding(.bottom,50)
     }
 }
+
