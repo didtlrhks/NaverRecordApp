22ecf567835fdbfcbe54591f8d8397ab195788b6
sikwan <yysskk99999@naver.com>
2024-01-20 20:01:21 +0900

    TodoViewModel class ,extension하기

--- NaverRECORDAPP/NaverRECORDAPP/TodoViewModel.swift
+++ NaverRECORDAPP/NaverRECORDAPP/TodoViewModel.swift
@@ -12,7 +12,7 @@ class TodoViewModel : ObservableObject {
     @Published var time : Date
     @Published var isDisplayCalendar : Bool
     @Published var day : Date
-    
+    // ui의 상태를 최신화해주는 published를 사용해준다.
     init(title: String = "",
          time: Date = Date(),
          isDisplayCalendar: Bool = false,
@@ -31,6 +31,6 @@ extension TodoViewModel {
     func setIsDisplayCalendar(
         _ isDispaly : Bool
     ){
-        isDisplayCalendar = isDispaly
+        isDisplayCalendar = isDispaly // isDisplay의 값을 받아서 isDisplayCalendar 속성에 할당할거임 이제 이런게 동작을 캡슐화했다고 봐도 맞는거지
     }
 }
